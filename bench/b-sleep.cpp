dcf53a5331949d5dccf477d412dfe40f677fe052
Borislav Stanimirov <b@ibob.bg>
2025-11-04 11:31:56 +0200

    bench: compare sleep results

--- bench/b-sleep.cpp
+++ bench/b-sleep.cpp
@@ -59,6 +59,8 @@ int main(int argc, char* argv[]) {
 
     picobench::runner r;
     r.set_default_state_iterations({10, 20});
+    r.set_compare_results_across_samples(true);
+    r.set_compare_results_across_benchmarks(true);
     r.parse_cmd_line(argc, argv);
 
     return r.run();
