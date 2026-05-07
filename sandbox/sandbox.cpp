b28578ea3e5a18ccd0e0d6de000e5cabb281896a
Borislav Stanimirov <b@ibob.bg>
2026-05-07 21:52:15 +0300

    sandbox: experimenting with a potential deadlock

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -4,6 +4,26 @@
 #include <iostream>
 #include <functional>
 
+//inline void init_benchmark(uint32_t num_jobs) {
+//    par::thread_pool::init_global(std::min(std::thread::hardware_concurrency(), num_jobs - 1));
+//
+//    num_jobs = par::thread_pool::global().num_threads() + 1;
+//
+//    std::atomic_uint32_t counter{0};
+//
+//    // warm up par
+//    [[maybe_unused]] auto ret = par::thread_pool::global().warmup();
+//
+//    // warm up OpenMP
+//    #pragma omp parallel for num_threads(num_jobs) schedule(static)
+//    for (int i = 0; i < int(num_jobs); ++i) {
+//        ++counter;
+//    }
+//
+//    // sanity
+//    assert(ret == counter);
+//}
+
 int main() {
     //pfor(pool, {.max_par = 4}, 0, 10,
     //    [](job_info job) {
