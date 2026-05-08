dd9d6b9c2788bb63cbbf3dcaa7f4bf4ab1e734c6
Borislav Stanimirov <b@ibob.bg>
2026-05-08 09:49:20 +0300

    bench: proper num threads based on num jobs (previously spawned +1)

--- bench/bu-init.hpp
+++ bench/bu-init.hpp
@@ -6,22 +6,27 @@
 #include <omp.h>
 #include <atomic>
 #include <thread>
+#include <cassert>
 
-inline void init_benchmark(uint32_t num_threads) {
-    par::thread_pool::init_global(std::min(std::thread::hardware_concurrency(), num_threads));
+inline void init_benchmark(uint32_t num_jobs) {
+    par::thread_pool::init_global(std::min(std::thread::hardware_concurrency(), num_jobs - 1));
 
-    num_threads = par::thread_pool::global().num_threads();
+    // recompute to actual number of available jobs
+    num_jobs = par::thread_pool::global().num_threads() + 1;
+
+    // warm up par
+    [[maybe_unused]] auto par_jobs = par::thread_pool::global().warmup();
+
+    // warm up OpenMP
 
     std::atomic_uint32_t counter{0};
 
-    // warm up par
-    par::prun({.sched = par::schedule_static}, [&](uint32_t) {
-        ++counter;
-    });
-
-    // warm up OpenMP
-    #pragma omp parallel for num_threads(num_threads) schedule(static)
-    for (int i = 0; i < int(num_threads); ++i) {
+    // (num_threads includes the caller in OpenMP)
+    #pragma omp parallel for num_threads(num_jobs) schedule(static)
+    for (int i = 0; i < int(num_jobs); ++i) {
         ++counter;
     }
+
+    // sanity
+    assert(par_jobs == counter);
 }
